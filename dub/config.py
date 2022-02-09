88d33bdacd8978f2fd9bc2fdc2c54aa0d6d08c36
Braunly <braunly.brony@gmail.com>
2022-02-09 15:00:05 +0200

    Add more env vars for server meta config

--- dub/config.py
+++ dub/config.py
@@ -18,5 +18,12 @@ MEDIA_ROOT = environ.str("MEDIA_ROOT", "media/")
 # Database connections
 DB_URL = environ.str("DB_URL", "mongodb://{user}:{password}@{host}:{port}/{db}")
 
+# Server name in main menu
+SERVER_NAME = environ.str("SERVER_NAME", "Server name")
+
+# Skins domain
+SKIN_DOMAIN = environ.str("SKIN_DOMAIN", ".example.com")
+
+# RSA keys
 PRIVATE_RSA_KEY = b64decode(environ.str('PRIVATE_RSA_KEY'))
 PUBLIC_RSA_KEY = b64decode(environ.str('PUBLIC_RSA_KEY'))
--- dub/resources/root.py
+++ dub/resources/root.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, current_app
 
 root_blueprint = Blueprint(
     'root',
@@ -11,11 +11,11 @@ root_blueprint = Blueprint(
 def root():
     return {
             "meta": {
-                "serverName": "Server name",
+                "serverName": current_app.config.get("SERVER_NAME", "Server name"),
                 "feature.non_email_login": True
             },
             "skinDomains": [
-                "example.com"
+                current_app.config.get("SKIN_DOMAIN", ".example.com")
             ],
-            "signaturePublickey": ""
+            "signaturePublickey": current_app.config.get("PUBLIC_RSA_KEY")
         }
